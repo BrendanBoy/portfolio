f1bc0352d2c7bb38692b263cd65360eed2390bea
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2021-12-08 15:02:39 +0000

    Add typing effect to the banner section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-const testing = document.getElementById('test-type');
+const headType = document.getElementById('head-type');
+const paraType = document.getElementById('para-type');
 
-const typewriter = new Typewriter(testing, {
-    loop: true,
-    delay: 75,
+const headWriter = new Typewriter(headType, {
+    autoStart: true,
+    cursor: null,
+    strings: 'My Name is Brendan Boyle'
 });
 
-typewriter
-    .typeString('Testing')
+const paraWriter = new Typewriter(paraType, {
+    delay: 75
+});
+
+// Can I make this DRYer?
+paraWriter
+    .pauseFor(4500)
+    .changeDeleteSpeed(1)
+    .typeString("I'm a Pirate!")
     .pauseFor(300)
     .deleteChars(7)
-    .typeString('Testing')
+    .typeString('Ninja')
     .pauseFor(300)
-    .typeString(' 1')
+    .deleteChars(5)
+    .typeString('Spaceman?')
     .pauseFor(300)
-    .typeString(', 2')
-    .pauseFor(300)
-    .typeString(', 3')
-    .pauseFor(2000)
+    .deleteChars(9)
+    .typeString('<strong>Web Developer</strong>')
     .start();
