becd0f10db067cb7475fbe3c9927d0a4ad1252c6
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2021-12-13 04:13:40 +0000

    Improve telephone number regex

--- js/form-validation.js
+++ js/form-validation.js
@@ -7,7 +7,7 @@ const userPhone = document.getElementById('user_phone');
 const userMessage = document.getElementById('user_message');
 
 const regEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-const regTel = /\d{11}/;
+const regTel = /^(?:0|\+?44)(?:\d\s?){9,10}$/;
 
 function chkMail(email) {
     return regEmail.test(email);
