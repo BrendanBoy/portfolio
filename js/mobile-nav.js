498a96a9427b8df0a37bd329542d77f14c656fa4
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-02-04 14:48:57 +0000

    Write a function that closes the Mobile Navigation when the screen is resized to widths of 768px and above & add a debounce function to reduce the frequency of this function

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -10,3 +10,26 @@ menuButton.addEventListener('click', function() {
 mobileNav.addEventListener('click', function() {
     mobileNav.classList.toggle('is-active');
 });
+
+const debounce = (func, wait, immediate) => {
+	var timeout;
+	return function() {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+var screenCheck = debounce(function() {
+    if (screen.width >= 768) {
+        mobileNav.classList.remove('is-active');
+    }
+}, 250);
+
+window.addEventListener('resize', screenCheck);
