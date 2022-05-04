3c712bfe1a09d3a3648cc3fa4c2fff85254739e4
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-05-04 10:45:28 +0100

    Split the head section of index.php into it's own PHP file

---
+++ inc/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noticia+Text&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/3f8b63cd88.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/prism.css">
+    <title><?php echo $pageTitle ?></title>
+    <meta name="description" content="I am Brendan Boyle, a Junior Web Developer, this is my portfolio containing project examples.">
+</head>
+<body>
+    <div id="overlay" onclick="closeNav()"></div>
--- index.php
+++ index.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noticia+Text&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"> 
-    <script src="https://kit.fontawesome.com/3f8b63cd88.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Brendan Boyle | Portfolio</title>
-    <meta name="description" content="I am Brendan Boyle, a Junior Web Developer, this is my portfolio containing project examples.">
-</head>
-<body>
-    <div id="overlay" onclick="closeNav()"></div>
+<?php
+$pageTitle = "Brendan Boyle | Portfolio";
+include("inc/head.php");
+?>
+
+    <!-- main page only -->
     <div class="header-bar home-bar"></div>
     <button class="mobile-btn" onclick="openNav()">
         <i class="fas fa-bars"></i>
     </button>
+    <!-- main page only -->
     <nav class="mobile-nav">
         <div class="initials">
             <a href="index.php">BB</a>
