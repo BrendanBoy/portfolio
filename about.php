83c94ee15b1711bcbd1f8950adee82e3a165fde2
Brendan Boyle <brendan.boyle@netmatters-scs.com>
2022-05-05 14:30:28 +0100

    Fix broken link to SCS in the About Me section

--- about.php
+++ about.php
@@ -14,7 +14,7 @@ include("inc/sub-page-header.php");
             <p>I have always tried to utilise these skills in the workplace, whether it's updating or maintaining the companys website or managing the organisations social media presence.</p>
             <p>The largest project I have worked on to date was as a freelance programmer for Videotel, building test modules for an interactive training package for the maritime industry.</p>
             <p>Following redundancy at the end of 2020 I decided to further pursue my interest in web development, a field that allows me to combine my fascination with technology and my creative flair.</p>
-            <p>To further this aim I have been participating in the <a href="scs.html#scs">Scion Coalition Scheme</a>, an intensive training program designed to fully prepare candidates to enter this industry.</p>
+            <p>To further this aim I have been participating in the <a href="scs.php#scs">Scion Coalition Scheme</a>, an intensive training program designed to fully prepare candidates to enter this industry.</p>
         </section>
     </main>
     <?php include("inc/sub-page-footer.php"); ?>
